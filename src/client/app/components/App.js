5475dd9ef0e119b28a82e40e30d62ffc216d5c31
xiaodong <xiaodong.li@asiainnovations.com>
2016-07-07 15:38:43 +0800

    update

---
+++ src/client/app/components/App.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lixiaodong on 16/7/7.
+ */
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
+
+const App = () => (
+    <div>
+        <AddTodo />
+        <VisibleTodoList />
+        <Footer />
+    </div>
+)
+
+export default App;
---
+++ src/client/app/reducers/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by lixiaodong on 16/7/7.
+ */
+import { combineReducers } from 'redux'
+import todos from './todos'
+import visibilityFilter from './visibilityFilter'
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+})
+
+export default todoApp;
