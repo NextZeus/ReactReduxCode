250491fa20947c0b6803ec25ac4b370a80dce14a
xiaodong <875697126@qq.com>
2016-07-14 13:43:36 +0800

    add async actions

---
+++ src/tutorial/reduxAsyncActions.js
@@ -0,0 +1,74 @@
+/**
+ * Created by lixiaodong on 16/7/14.
+ */
+import { applyMiddlerware, createStore } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import promise from 'redux-promise-middleware';
+
+const initialState = {
+    fetching:false,
+    fetched:false,
+    users:[],
+    error:null
+}
+
+const reducer = function (state=initialState, action) {
+    switch (action.type){
+        case 'FETCH_USERS_START':
+            return {...state, fetching:true};
+            break;
+        case 'RECEIVER_USERS':
+            return {...state, fetching:false,fetched:true,data:action.payload};
+            break;
+        case 'FETCH_USERS_ERROR':
+            return {...state, fetching:false,fetched:false,error : action.payload};
+            break;
+    }
+
+    return state;
+};
+
+
+const middleware = applyMiddlerware(promise(), thunk,logger());
+
+const store = createStore( reducer ,middleware);//0 is initial state
+
+//store.dispatch((dispatch)=>{
+//    dispatch({type : 'FETCH_USER_START'});
+//
+//    axios.get('http://rest.learncode.academy/api/wstern/users')
+//    .then((response)=>{
+//        dispatch({type : 'RECEIVER_USERS', payload : response.data});
+//    })
+//    .catch((err)=>{
+//            dispatch({type : 'FETCH_USERS_ERROR', payload:err});
+//    });
+//});
+
+//promise
+store.dispatch({
+    type : 'FETCH_USERS',
+    payload:axios.get('http://rest.learncode.academy/api/wstern/users')
+});
+
+//promise reducer
+const reducer = function (state=initialState, action) {
+    switch (action.type){
+        case 'FETCH_USERS_PENDING':
+            return {...state, fetching:true};
+            break;
+        case 'FETCH_USERS_FULFILLED':
+            return {...state, fetching:false,fetched:true,data:action.payload};
+            break;
+        case 'FETCH_USERS_REJECTED':
+            return {...state, fetching:false,fetched:false,error : action.payload};
+            break;
+    }
+
+    return state;
+};
+
+
+store.dispatch({type : 'FOO', payload:1});
--- src/tutorial/reduxMiddleware.js
+++ src/tutorial/reduxMiddleware.js
@@ -1,3 +1,41 @@
 /**
  * Created by lixiaodong on 16/7/14.
  */
+import { applyMiddlerware, createStore } from 'redux';
+
+const reducer = function (state, action) {
+    if(action.type === 'INC'){
+        return state + 1;
+    } else if(action.type === 'DEC'){
+        return state - 1;
+    } else if(action.type === 'E'){
+        throw new Error('Error!!!!');
+    }
+    return state;
+}
+
+const logger = (store)=>(next)=>(action)=>{
+    console.log('action fired',action);
+    next(action);
+}
+
+const error = (store)=>(next)=>(action)=>{
+    try{
+        next(action);
+    } catch(e){
+        console.log('AAAAAAA');
+    }
+}
+
+const middleware = applyMiddlerware(logger,error);
+
+const store = createStore( reducer, 0 ,middleware);//0 is initial state
+
+store.subscribe(()=>{
+    console.log('store changed',store.getState());
+});
+
+store.dispatch({type : 'INC', payload:1});
+store.dispatch({type : 'INC', payload:100});
+store.dispatch({type : 'INC', payload:22});
+store.dispatch({type : 'E', payload:10});
